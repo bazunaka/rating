08d2f029534fd02db8288bca5fff6030e2857a85
bazunaka <40122285+bazunaka@users.noreply.github.com>
2021-01-19 22:54:25 +0300

    new project

---
+++ rating/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace rating
+{
+    public partial class Form1 : Form
+    {
+
+        private SQLiteConnection connect;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect = new SQLiteConnection("Data Source=pers_rating.db; Version=3");
+            connect.Open();
+
+            SQLiteCommand command = connect.CreateCommand();
+            command.CommandText = "SELECT indicators.name_indicator FROM indicators";
+            Console.WriteLine(command);
+        }
+    }
+}
---
+++ rating/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+
+namespace rating
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+        }
+    }
+}
